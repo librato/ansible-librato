fc5dd6e8cb92fc0fd2666b502f5b076a30cef5fa
Mike Julian <mike@mikejulian.com>
2016-12-20 12:25:36 -0800

    Fix missing config stanzas plus tests

--- test/integration/default/serverspec/mongodb_spec.rb
+++ test/integration/default/serverspec/mongodb_spec.rb
@@ -3,6 +3,7 @@ set :backend, :exec
 
 describe file('/opt/collectd/etc/collectd.conf.d/mongodb.conf') do
   it { should exist }
+  it { should contain 'LoadPlugin python' }
   it { should contain 'Host "localhost"' }
   it { should contain 'Port "27017"' }
   it { should contain 'Database "admin", "foo", "bar", "baz"' }
--- test/integration/default/serverspec/nginx_spec.rb
+++ test/integration/default/serverspec/nginx_spec.rb
@@ -3,5 +3,6 @@ set :backend, :exec
 
 describe file('/opt/collectd/etc/collectd.conf.d/nginx.conf') do
   it { should exist }
+  it { should contain 'LoadPlugin nginx' }
   it { should contain 'URL "http://localhost/test-status"' }
 end
